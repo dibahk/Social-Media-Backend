2e4b9380d5c1a279d5fee9d4222b2345eb2af327
dibahk <dibahadjikazemi@gmail.com>
2024-09-09 01:51:50 +0100

    adding email checker

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class PostBase(BaseModel):
@@ -17,5 +17,5 @@ class Post(PostBase):
         orm_mode= True
 
 class UserCreate(BaseModel):
-    email: str
+    email: EmailStr
     password: str
