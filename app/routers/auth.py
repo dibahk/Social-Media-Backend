95f7ccce01811fab2fdba94af3b2e92a4f9f7ed8
dibahk <dibahadjikazemi@gmail.com>
2024-09-19 19:06:57 +0100

    adding authorization

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,10 +10,10 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(),db: Session= D
     # oauth2passwordrequestform always senses username
     user = db.query(models.User).filter(models.User.email==user_credentials.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_NOT_FOUND, detail= f"Invalid credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_NOT_FOUND, detail="invalid credentials")
     
     access_token = oauth2.create_access_token(data= {"user_id": user.id})
 
