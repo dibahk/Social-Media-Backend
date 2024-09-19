d99b7109ce05882e388635969d84096bf5ca5acc
dibahk <dibahadjikazemi@gmail.com>
2024-09-19 21:48:21 +0100

    finishing touches of authentication

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-
+from sqlalchemy.orm import Session
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 #SECRET_KEY
@@ -34,12 +34,13 @@ def verify_access_token(token:str, crendtials_exception):
         token_data = schemas.TokenData(id=id)
     except JWTError:
         raise crendtials_exception
-
     
     return token_data
 
 
-def get_current_user(token: str =Depends(oauth2_scheme)):
+def get_current_user(token: str =Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail=f"could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
